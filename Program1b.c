825521c0525c427b2d75ebafbd61ccc26512d79d
Karan Arora <40701579+DreamITJob@users.noreply.github.com>
2018-07-02 14:02:58 +0530

    Create Program1b.c

---
+++ Program1b.c
@@ -0,0 +1,33 @@
+//Program to find maximum number among 3 numbers using if-else statements in C....
+
+#include <stdio.h>
+
+int main()
+{
+    int number1;
+    int number2;
+    int number3;
+    printf("Enter first number ");
+    scanf("%d",&number1);
+    printf("Enter second number ");
+    scanf("%d",&number2);
+    printf("Enter third number ");
+    scanf("%d",&number3);
+    //find maximum number with if else statments..
+    if (number1>number2)
+    {
+        if (number1>number3)
+            printf("Greatest integer is %d", number1);
+        else
+            printf("Greatest integer is %d", number3);
+    }
+    else
+    {
+        if (number2>number3)
+            printf("Greatest integer is %d", number2);
+        else
+            printf("Greatest integer is %d", number3);
+    }
+        
+    return 0;
+}
