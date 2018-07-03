f38b8fbfb9b02bd951d49d9961409ceed964dea8
Karan Arora <40701579+DreamITJob@users.noreply.github.com>
2018-07-03 12:48:58 +0530

    Create Program9.c

---
+++ Program9.c
@@ -0,0 +1,26 @@
+//C program for Fibonacci Sequence Up to a Certain Number....
+
+#include <stdio.h>
+
+int main()
+{
+    int term1 = 0, term2 = 1, nextTerm = 0, num;
+
+    printf("Enter a positive number: ");
+    scanf("%d", &num);
+
+    // Displays the first two terms which is always 0 and 1
+    printf("Fibonacci Series: %d, %d, ", term1, term2);
+
+    nextTerm = term1 + term2;
+
+    while(nextTerm <= num)
+    {
+        printf("%d, ",nextTerm);
+        term1 = term2;
+        term2 = nextTerm;
+        nextTerm = term1 + term2;
+    }
+
+    return 0;
+}
