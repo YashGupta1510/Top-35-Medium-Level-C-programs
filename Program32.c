4aeeb32de7bae9691dd9e44b77f9024e0696ef71
Karan Arora <40701579+DreamITJob@users.noreply.github.com>
2018-07-07 21:13:44 +0530

    Create Program32.c

---
+++ Program32.c
@@ -0,0 +1,8 @@
+// Following are the links for sorting programs in C:
+
+
+//insertionm sort : https://www.geeksforgeeks.org/insertion-sort/
+//selection sort : https://www.geeksforgeeks.org/selection-sort/
+//bubble sort : https://www.geeksforgeeks.org/bubble-sort/
+//quick sort : https://www.geeksforgeeks.org/quick-sort/
+//merge sort : https://www.geeksforgeeks.org/merge-sort/
